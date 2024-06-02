0b91952aea372d4f8a12ea1bb9158e2664e93a74
Sharan Varma <sharan.varma@skit.ai>
2024-06-02 11:45:43 +0530

    Proliferate fixes from graph_struct.go to Breadth First Search file

--- graph_algos/bfs_graph.go
+++ graph_algos/bfs_graph.go
@@ -7,49 +7,65 @@ import (
 
 var distance []int
 var predecessor []int
-var queue []*graph.GraphNode
+var queue []*graph.Vertex
 
-func constructSampleGraph() []*graph.GraphNode {
-    node_values := []int{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
-    graph.NewGraph()
-    nodelist := make([]*graph.GraphNode, 0)
-    for _, value := range node_values {
-        nodelist = append(nodelist, graph.InitGraphNode(value, []*graph.GraphNode{}, "white"))
+func constructSampleGraph() *graph.Graph {
+    graph_map := map[int][]map[int]int{
+        0: {
+            map[int]int{1: 0}, 
+            map[int]int{6: 0},
+            map[int]int{8: 0},
+            },
+        1: {
+            map[int]int{2: 0},
+            map[int]int{3: 0},
+        },
+        2: {
+            map[int]int{10: 0},
+            map[int]int{11: 0},
+        },
+        3: {
+            map[int]int{4: 0},
+            map[int]int{12: 0},
+        },
+        4: {
+            map[int]int{5: 0},
+            map[int]int{13: 0},
+        },
+        5: {
+            map[int]int{6: 0},
+            map[int]int{9: 0},
+        },
+        6: {
+            map[int]int{7: 0},
+        },
+        7: {
+            map[int]int{8: 0},
+            map[int]int{9: 0},
+        },
+        8: {
+            map[int]int{14: 0},
+        },
+        9: {
+            map[int]int{15: 0},
+        },
     }
     
-    graph.ConnectNodes(nodelist[0], nodelist[1])
-    graph.ConnectNodes(nodelist[0], nodelist[6])
-    graph.ConnectNodes(nodelist[0], nodelist[8])
-    graph.ConnectNodes(nodelist[1], nodelist[2])
-    graph.ConnectNodes(nodelist[1], nodelist[3])
-    graph.ConnectNodes(nodelist[2], nodelist[10])
-    graph.ConnectNodes(nodelist[2], nodelist[11])
-    graph.ConnectNodes(nodelist[3], nodelist[4])
-    graph.ConnectNodes(nodelist[3], nodelist[12])
-    graph.ConnectNodes(nodelist[4], nodelist[5])
-    graph.ConnectNodes(nodelist[4], nodelist[13])
-    graph.ConnectNodes(nodelist[5], nodelist[6])
-    graph.ConnectNodes(nodelist[5], nodelist[9])
-    graph.ConnectNodes(nodelist[6], nodelist[7])
-    graph.ConnectNodes(nodelist[7], nodelist[8])
-    graph.ConnectNodes(nodelist[7], nodelist[9])
-    graph.ConnectNodes(nodelist[8], nodelist[14])
-    graph.ConnectNodes(nodelist[9], nodelist[15])
-    
-    for _, node := range nodelist {
-        graph.InsertGraphNode(node)
-    }
-    graph.PrintGraph()
-    return nodelist
+    newGraph := graph.ConstructGraphFromMap(graph_map)
+    newGraph.Print()
+    return newGraph
 }
 
-func bfs_visit() {
+func bfs_visit(graphObj *graph.Graph) {
     for len(queue) > 0 {
         node := queue[0]
         queue = queue[1:]
         
-        for _, neighbour := range node.GetConnectedNodes() {
-            if neighbour.GetColor() != "white" {
+        for _, edge := range graphObj.Edges[node] {
+            neighbour := edge.GetDestination()
+            fmt.Printf("Current Node Value: %d, edge Destination Value: %d\n", node.GetValue(), neighbour.GetValue())
+            fmt.Printf("Source Color: %s Neighbour Color: %s\n", node.GetColor(), neighbour.GetColor())
+            if neighbour.GetColor() == "black" {
                 continue
             }
             predecessor[neighbour.GetValue()] = node.GetValue()
@@ -64,17 +80,18 @@ func bfs_visit() {
 
 func main() {
     distance, predecessor = make([]int, 0), make([]int, 0)
-    queue = make([]*graph.GraphNode, 0)
-    node_list := constructSampleGraph()
-    for range node_list {
+    queue = make([]*graph.Vertex, 0)
+    initGraph := constructSampleGraph()
+    graphVertices := initGraph.Vertices
+    for range graphVertices {
         distance = append(distance, -1)
         predecessor = append(predecessor, -1)
     }
-    startingNode := node_list[0]
+    startingNode := initGraph.GetVertexWithValue(0)
     distance[startingNode.GetValue()] = 0
     startingNode.SetColor("gray")
     queue = append(queue, startingNode)
-    bfs_visit()
+    bfs_visit(initGraph)
 
     fmt.Printf("Queue: %v\n Distance: %v\n Prececessors: %v\n", queue, distance, predecessor)
 }
