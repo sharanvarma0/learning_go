d1917f329cb332bea504a9411a390949b4708c49
Sharan Varma <sharan.varma@skit.ai>
2024-06-02 11:45:08 +0530

    Fix: Logic for graph creation. Add extra functions for graph operation. Fix existing pointer functions and data structures

--- graph/graph_struct.go
+++ graph/graph_struct.go
@@ -17,7 +17,7 @@ type Edge struct {
 
 type Graph struct {
     Vertices []*Vertex
-    Edges map[Vertex][]*Edge
+    Edges map[*Vertex][]*Edge
     VertexValuesMap map[int]*Vertex
 }
 
@@ -33,6 +33,16 @@ func (v *Vertex) GetColor() string {
     return v.color
 }
 
+func (v *Vertex) SetColor(color string) bool {
+    v.color = color
+    return true
+}
+
+func (v *Vertex) SetValue(value int) bool {
+    v.value = value
+    return true
+}
+
 func (e *Edge) GetDestination() *Vertex {
     return e.destination
 }
@@ -44,70 +54,98 @@ func (e *Edge) GetWeight() int {
 func InitGraph() *Graph {
     return &Graph{
         Vertices: make([]*Vertex, 0),
-        Edges: make(map[Vertex][]*Edge),
+        Edges: make(map[*Vertex][]*Edge),
         VertexValuesMap: make(map[int]*Vertex),
     }
 }
 
-func GetVertex(value int, color string) *Vertex {
+func CreateNewVertex(value int, color string) *Vertex {
     return &Vertex{
         color: color,
         value: value,
     }
 }
 
-func GetEdge(destination *Vertex, weight int) *Edge {
+func CreateNewEdge(destination *Vertex, weight int) *Edge {
     return &Edge{
         destination: destination,
         weight: weight,
     }
 }
 
-func (g *Graph) AddVertex(v GraphVertex) (bool, error) {
+
+func (g *Graph) GetVertexWithValue(value int) *Vertex {
+    vertexPtr, exists := g.VertexValuesMap[value]
+    if !exists {
+        fmt.Printf("%d Vertex does not exist in the graph. Please create it if required")
+        return nil
+    }
+    return vertexPtr
+}
+    
+
+func (g *Graph) AddVertexToGraph(v GraphVertex) (*Vertex, error) {
     vertexValue := v.GetValue()
-    _, exists := g.VertexValuesMap[vertexValue]
+    existingVertex, exists := g.VertexValuesMap[vertexValue]
     if exists {
         errMsg := fmt.Sprintf("%d already present as graph vertex. Returning", vertexValue)
-        return false, errors.New(errMsg)
+        return existingVertex, errors.New(errMsg)
     }
-    newVertex := &Vertex{
-        value: vertexValue,
-        color: "black",
-    }
+    newVertex := CreateNewVertex(vertexValue, "white")
     g.Vertices = append(g.Vertices, newVertex)
     g.VertexValuesMap[vertexValue] = newVertex
-    return true, nil
+    return newVertex, nil
 }
 
-func (g *Graph) AddEdge (source *Vertex, destination *Vertex, weight int) (bool, error) {
-    currentEdges, exists := g.Edges[*source]
-    newEdge := &Edge{
-        destination: destination,
-        weight: weight,
-    }
-    if !exists {
-        g.Edges[*source] = make([]*Edge, 0)
-        g.Edges[*source] = append(g.Edges[*source], newEdge)
-        return true, nil
-    }
-    
-    if exists {
-        for _, edge := range currentEdges {
-            if destination == edge.destination {
-                errMsg := fmt.Sprintf("%d -> %d already present", source.GetValue(), destination.GetValue())
-                return false, errors.New(errMsg)
-            }
+
+func (g *Graph) checkForExistingEdge(source, destination *Vertex) (srcToDest bool, destToSrc bool) {
+    for _, edge := range g.Edges[source] {
+        if destination == edge.destination {
+            srcToDest = true
         }
     }
-    
-    g.Edges[*source] = append(g.Edges[*source], newEdge)
+    for _, edge := range g.Edges[destination] {
+        if edge.destination == source {
+            destToSrc = true
+        }
+    }
+
+    return
+}
+
+func (g *Graph) AddEdgeToGraph(source *Vertex, destination *Vertex, weight int) (bool, error) {
+    _, exists := g.Edges[source]
+    _, destEdgesExists := g.Edges[destination]
+    if !exists {
+        g.Edges[source] = make([]*Edge, 0)
+    }
+    if !destEdgesExists {
+        g.Edges[destination] = make([]*Edge, 0)
+    }
+   
+    srcToDest, destToSrc := g.checkForExistingEdge(source, destination)
+
+    if !srcToDest {
+        newEdge := CreateNewEdge(destination, weight)
+        g.Edges[source] = append(g.Edges[source], newEdge)
+    }
+
+    if weight == 0 {
+        if !destToSrc {
+            destinationEdge := CreateNewEdge(source, weight)
+            g.Edges[destination] = append(g.Edges[destination], destinationEdge)
+            fmt.Println("Weight Passed: 0. Added Bidirectional Edge")
+        }
+    }
     return true, nil
 }
 
 func (g *Graph) Print() {
+    fmt.Printf("Vertices: %v\n Edges: %v\n", g.Vertices, g.Edges)
     for key, vertexObj := range g.VertexValuesMap {
         fmt.Printf("%d: ", key)
-        edges := g.Edges[*vertexObj]
+        edges := g.Edges[vertexObj]
+        fmt.Printf("Edges: %v\n", edges)
         for _, edge := range edges {
             destinationValue := edge.GetDestination().GetValue()
             destinationWeight := edge.GetWeight()
@@ -118,21 +156,23 @@ func (g *Graph) Print() {
 }
             
 
-func ConstructGraphFromMap(vertexEdgeWeightMap map[int][]map[int]int) *Graph {
+func ConstructGraphFromMap(vertexEdgeMap map[int][]map[int]int) *Graph {
     newGraph := InitGraph()
-    for value, edges := range vertexEdgeWeightMap {
-        vertex := GetVertex(value, "black")
-        _, vertexError := newGraph.AddVertex(vertex)
+    for value, edges := range vertexEdgeMap {
+        vertex := CreateNewVertex(value, "white")
+        srcVertexPtr, vertexError := newGraph.AddVertexToGraph(vertex)
         if vertexError != nil {
-            fmt.Printf("%s\n", vertexError)
+            fmt.Printf("Vertex Creation Error: %s\n", vertexError)
         }
         for _, edge := range edges {
             for destinationValue, weight := range edge {
-                destinationVertex := GetVertex(destinationValue, "black")
-                destinationVertexCreated, destinationVertexError := newGraph.AddVertex(destinationVertex)
-                fmt.Printf("%d Vertex Created: %v, Vertex Creation Error: %v\n", destinationValue, destinationVertexCreated, destinationVertexError)
-                edgeCreated, edgeError := newGraph.AddEdge(vertex, destinationVertex, weight)
-                fmt.Printf("%d -> %d Edge Created: %v, Edge Creation Error: %v\n", vertex.value, destinationVertex.value, edgeCreated, edgeError)
+                destinationVertex := CreateNewVertex(destinationValue, "white")
+                destinationVertexPtr, destinationVertexError := newGraph.AddVertexToGraph(destinationVertex)
+                fmt.Printf("%d Vertex Created, Vertex Creation Error: %v\n", destinationValue, destinationVertexError)
+                if destinationVertexPtr != nil {
+                    edgeCreated, edgeError := newGraph.AddEdgeToGraph(srcVertexPtr, destinationVertexPtr, weight)
+                    fmt.Printf("%d -> %d Edge Created: %v, Edge Creation Error: %v\n", vertex.value, destinationVertex.value, edgeCreated, edgeError)
+                }
             }
         }
     }
